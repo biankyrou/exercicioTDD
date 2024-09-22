d78378700564ea33ef8bae646c10ab3ef1b8c494
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-09-22 15:24:13 -0300

    feat: adding test class ExTemperaturaSemanaTest and testing first case

--- src/test/java/exercicio/ExTemperaturaSemanaTest.java
+++ src/test/java/exercicio/ExTemperaturaSemanaTest.java
@@ -1,4 +1,14 @@
 package exercicio;
 
+import br.edu.ifsp.exercicio.ExTemperaturaSemana;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
 public class ExTemperaturaSemanaTest {
+    @Test
+    public void testDiasAcimaDaMediaUm() {
+        int[] temperaturas = {2, 2, 2, 2, 2, 2, 3};
+        assertEquals("1", ExTemperaturaSemana.quantosDiasSemanaTempAcimaMedia(temperaturas));
+    }
 }
