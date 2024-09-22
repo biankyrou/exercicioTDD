b26ab471cfd26d4ba4fa0a46ee2deff2ccf48770
biankyrou <bianca.carvalio@aluno.ifsp.edu.br>
2024-09-22 15:28:22 -0300

    test: add testing for three

--- src/test/java/exercicio/ExTemperaturaSemanaTest.java
+++ src/test/java/exercicio/ExTemperaturaSemanaTest.java
@@ -17,4 +17,10 @@ public class ExTemperaturaSemanaTest {
         int[] temperaturas = {21, 10, 13, 34, 30, 21, 34};
         assertEquals("3", ExTemperaturaSemana.quantosDiasSemanaTempAcimaMedia(temperaturas));
     }
+
+    @Test
+    public void testDiasAcimaDaMediaTres() {
+        int[] temperaturas = {2, 2, 2, 2, 2, 2, 1};
+        assertEquals("6", ExTemperaturaSemana.quantosDiasSemanaTempAcimaMedia(temperaturas));
+    }
 }
